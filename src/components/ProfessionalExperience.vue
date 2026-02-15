b810bf416e587d5e72d2086f8ef05c1ad4efe1c7
Michel Philippe Edubas <mikeedubas@hotmail.com>
2026-02-15 18:10:21 +1000

    need to update the professional experience area

--- src/components/ProfessionalExperience.vue
+++ src/components/ProfessionalExperience.vue
@@ -4,34 +4,39 @@
     <div class="flex flex-col gap-6">
       <div
         v-for="experience in experiences"
-        :key="experience.title"
+        :key="experience.company"
         class="grid grid-cols-2 gap-y-4 border-b pb-4"
       >
         <div class="flex flex-col font-semibold">
           <span>
             {{ experience.company }}
           </span>
+        </div>
+
+        <div v-for="position in experience.positions" :key="position.title">
           <span class="text-sm">
-            {{ experience.title }}
+            {{ position.title }}
           </span>
-        </div>
-        <div class="flex flex-col text-sm font-light italic ml-auto text-right">
-          <span>
-            {{ experience.timeline }}
-          </span>
-          <span>
-            {{ experience.location }}
-          </span>
-        </div>
-        <ul class="col-span-2 text-sm">
-          <li
-            v-for="reponsibility in experience.responsibilities"
-            :key="reponsibility"
-            class="list-disc list-inside"
+          <div
+            class="flex flex-col text-sm font-light italic ml-auto text-right"
           >
-            {{ reponsibility }}
-          </li>
-        </ul>
+            <span>
+              {{ position.timeline }}
+            </span>
+            <span>
+              {{ position.location }}
+            </span>
+          </div>
+          <ul class="col-span-2 text-sm">
+            <li
+              v-for="reponsibility in position.responsibilities"
+              :key="reponsibility"
+              class="list-disc list-inside"
+            >
+              {{ reponsibility }}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -40,6 +45,10 @@
 <script setup lang="ts">
 interface Experience {
   company: string;
+  positions: Position[];
+}
+
+interface Position {
   title: string;
   timeline: string;
   location: string;
@@ -49,69 +58,83 @@ interface Experience {
 const experiences: Experience[] = [
   {
     company: "SteelX",
-    title: "Full-Stack Developer",
-    timeline: "March 2024 - Present",
-    location: "Gold Coast, Australia",
-    responsibilities: [
-      "Fixing bugs and implementing new features",
-      "Collaborating with engineers and designers during projects",
-      "Deployment of the system during releases",
-    ],
-  },
-  {
-    company: "Eippa Oy",
-    title: "Operations Department Manager",
-    timeline: "May 2021 - July 2021",
-    location: "Tallinn, Estonia",
-    responsibilities: [
-      "Building the logistics department from scratch in a new factory",
-      "Building the infrastructure for the department in relation to hardware and software",
-      "Building the team and training them",
-      "By the end of the project, the departmen was fully operational and was fufilling 2000 orders a day",
+    positions: [
+      {
+        title: "Full-Stack Developer",
+        timeline: "March 2024 - Present",
+        location: "Gold Coast, Australia",
+        responsibilities: [
+          "Fixing bugs and implementing new features",
+          "Collaborating with engineers and designers during projects",
+          "Deployment of the system during releases",
+        ],
+      },
     ],
   },
   {
     company: "Swappie Oy",
-    title: "Operations Team Lead",
-    timeline: "June 2020 - May 2021",
-    location: "Helsinki, Finland",
-    responsibilities: [
-      "Logistics team leader of the logistic department",
-      "Interviewing potential team members and onboarding them",
-      "Collaborating with other team leader to ensure smooth operations for our department and that KPI's are met",
-    ],
-  },
-  {
-    company: "Swappie Oy",
-    title: "Production Specialist",
-    timeline: "January 2020 - June 2020",
-    location: "Helsinki, Finland",
-    responsibilities: [
-      "Fixing iPhones",
-      "Packing orders",
-      "Quality Control of order",
+    positions: [
+      {
+        title: "Operations Department Manager",
+        timeline: "May 2021 - July 2021",
+        location: "Tallinn, Estonia",
+        responsibilities: [
+          "Building the logistics department from scratch in a new factory",
+          "Building the infrastructure for the department in relation to hardware and software",
+          "Building the team and training them",
+          "By the end of the project, the departmen was fully operational and was fufilling 2000 orders a day",
+        ],
+      },
+      {
+        title: "Operations Team Lead",
+        timeline: "June 2020 - May 2021",
+        location: "Helsinki, Finland",
+        responsibilities: [
+          "Logistics team leader of the logistic department",
+          "Interviewing potential team members and onboarding them",
+          "Collaborating with other team leader to ensure smooth operations for our department and that KPI's are met",
+        ],
+      },
+      {
+        title: "Production Specialist",
+        timeline: "January 2020 - June 2020",
+        location: "Helsinki, Finland",
+        responsibilities: [
+          "Fixing iPhones",
+          "Packing orders",
+          "Quality Control of order",
+        ],
+      },
     ],
   },
   {
     company: "K-Market",
-    title: "Store Clerk",
-    timeline: "February 2018 - December 2019",
-    location: "Helsinki, Finland",
-    responsibilities: [
-      "Ensuring that the store was in presentable state",
-      "Helping customer with their shopping needs and operating the register",
-      "Replinishing stock and variety of diffenrent responsibilies in the shop",
+    positions: [
+      {
+        title: "Store Clerk",
+        timeline: "February 2018 - December 2019",
+        location: "Helsinki, Finland",
+        responsibilities: [
+          "Ensuring that the store was in presentable state",
+          "Helping customer with their shopping needs and operating the register",
+          "Replinishing stock and variety of diffenrent responsibilies in the shop",
+        ],
+      },
     ],
   },
   {
     company: "Swissport",
-    title: "Customer Service Trainer",
-    timeline: "January 2017 - December 2018",
-    location: "Brisbane, Australia",
-    responsibilities: [
-      "Onboarding team members on company standard operating procedures",
-      "Training team members on a variety of check-in systems",
-      "Acting Shift Manager when required",
+    positions: [
+      {
+        title: "Customer Service Trainer",
+        timeline: "January 2017 - December 2018",
+        location: "Brisbane, Australia",
+        responsibilities: [
+          "Onboarding team members on company standard operating procedures",
+          "Training team members on a variety of check-in systems",
+          "Acting Shift Manager when required",
+        ],
+      },
     ],
   },
 ];
